65ca4294ba7a96db925a20f3eab24fff73639f36
Abishek George12 <abishek.george12@wsc.ac.uk>
2019-11-13 15:38:56 +0000

    Add project files.

---
+++ FlowDiagramControlLibrary/SymbolControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlowDiagramControlLibrary
+{
+    
+    /// <summary>
+    /// Interaction logic for SymbolControl.xaml
+    /// </summary>
+    public partial class SymbolControl : UserControl
+    {
+        private string currentSymbol;
+        public SymbolControl()
+        {
+            InitializeComponent();
+        }
+
+        public string CurrentSymbol { get => currentSymbol; set => currentSymbol = value; }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentSymbol = ((ComboBoxItem)cboSymbol.SelectedValue).Content.ToString();
+        }
+    }
+}
---
+++ FlowDiagramLibrary/Symbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowDiagramLibrary
+{
+    public class Symbol
+    {
+        public string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public Symbol()
+        {
+            this.name = "";
+        }
+
+
+        public Symbol(String name)
+        {
+            this.name = name;
+        }
+    }
+}
